279f6ec87e3c0c2a56471c174fa5403f8e26f54d
Riccardo Fonti <riccardo.fonti@studio.unibo.it>
2021-10-26 10:43:36 +0200

    implements method and field

--- src/it/unibo/oop/lab05/ex3/ProductImpl.java
+++ src/it/unibo/oop/lab05/ex3/ProductImpl.java
@@ -2,16 +2,25 @@ package it.unibo.oop.lab05.ex3;
 
 public class ProductImpl implements Product {
 
+	private final String name;
+	private double quantity;
+	
+	
+	
+	public ProductImpl(final String name, final int quantity) {
+		super();
+		this.name = name;
+		this.quantity = quantity;
+	}
+
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.name;
 	}
 
 	@Override
 	public double getQuantity() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.quantity;
 	}
 
 }
